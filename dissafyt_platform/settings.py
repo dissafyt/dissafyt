8951747c22907e45f5285b1b77b97aa74ac01863
dissafyt <dissafyt@gmail.com>
2026-03-13 16:57:46 +0000

    Initial push part 2

--- dissafyt_platform/settings.py
+++ dissafyt_platform/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hvg3r&^*&*!iu=4co_+zdse+e!=(ct6$!c8(2$5^%z+(3=_otx"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-hvg3r&^*&*!iu=4co_+zdse+e!=(ct6$!c8(2$5^%z+(3=_otx')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -91,11 +92,10 @@ WSGI_APPLICATION = "dissafyt_platform.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR}/db.sqlite3')
 }
 
 
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Celery Configuration
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
