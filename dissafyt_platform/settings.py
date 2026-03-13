83aeb015ea39c0a8d9fe637a1f8f1ea3eb69137f
dissafyt <dissafyt@gmail.com>
2026-03-13 17:19:24 +0000

    Initial push part 3

--- dissafyt_platform/settings.py
+++ dissafyt_platform/settings.py
@@ -33,8 +33,10 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(','
 
 SHARED_APPS = [
     'django.contrib.auth',
+    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'accounts',
 ]
