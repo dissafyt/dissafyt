1853d6f59643c4e0cbd3924e67ffdfdff3a7e45b
cl <cl@cls-MacBook-Air.local>
2026-03-17 00:44:48 +0200

    Use DATABASE_URL for Postgres on Render

--- dissafyt_platform/settings.py
+++ dissafyt_platform/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,6 +88,14 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# On Render, use DATABASE_URL to switch to Postgres
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.parse(
+        os.environ["DATABASE_URL"],
+        conn_max_age=600,
+        ssl_require=True,
+    )
 
 
 # Password validation
